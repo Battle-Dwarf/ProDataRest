4e60ff009ef11765323582ae776a069f08e7d893
Battle-Dwarf <marcin.dudek@battle-dwarf.org>
2018-04-29 20:31:22 +0200

    Remove hamrest dependency from POM Added step_1

--- src/test/java/JSONPlaceholder.java
+++ src/test/java/JSONPlaceholder.java
@@ -1,28 +1,26 @@
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Collections;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
 public class JSONPlaceholder {
 
     @Test
-    public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
+    public void step_1() {
 
-        given().
+        ArrayList<Integer> userIds =
+                given().
                 when().
-                get("http://ergast.com/api/f1/2017/circuits.json").
+                   get("http://jsonplaceholder.typicode.com/posts").
                 then().
-                assertThat().
-                body("MRData.CircuitTable.Circuits.circuitId", hasSize(20));
+                    extract().path("userId");
+
+        Integer max_uderId = Collections.max(userIds);
+        System.out.print(max_uderId);
     }
 
     @Test
-    public void test_Login() {
+    public void step_2() {
 
-        given().
-                when().
-                get("http://ergast.com/api/f1/2017/circuits.json").
-                then().
-                log().
-                body();
     }
 }
